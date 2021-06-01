ff0ef4d3feb1e9c14e0dc2185fb78a63cf251bf9
Junior de Oliveira <ellismarjr@hotmail.com>
2021-06-01 08:59:07 -0300

    create transactions list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
 
-
 import { Header } from '../../components/Header';
 import { HighlightCard } from '../../components/HighlightCard';
-
+import { TransactionCard, ITransactionCard } from '../../components/TransactionCard';
 
 import {
   Container,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
+  TrnsactionsList
 } from './styles';
 
+export interface DataListProps extends ITransactionCard {
+  id: string;
+}
+
 export function Dashboard() {
+  const data: DataListProps[] = [{
+    id: '1',
+    type: 'positive',
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: 'Vendas',
+      icon: 'dollar-sign'
+    },
+    date: "13/04/2020",
+  }, {
+    id: '2',
+    type: 'negative',
+    title: "Hamburguer Pizzy",
+    amount: "R$ 59,00",
+    category: {
+      name: 'Alimentação',
+      icon: 'coffee'
+    },
+    date: "10/04/2020",
+  }, {
+    id: '3',
+    type: 'negative',
+    title: "Aluguel do apartamento",
+    amount: "R$ 1.200,00",
+    category: {
+      name: 'Casa',
+      icon: 'shopping-bag'
+    },
+    date: "10/04/2020",
+  }];
+
   return (
     <Container>
       <Header />
@@ -35,6 +73,17 @@ export function Dashboard() {
           lastTransaction="01 à 16 de abril"
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TrnsactionsList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+
+      </Transactions>
+
     </Container>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import { RFPercentage } from 'react-native-responsive-fontsize';
+import { FlatList, Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+
+import { DataListProps } from '.';
 
 
 export const Container = styled.View`
@@ -19,3 +23,21 @@ export const HighlightCards = styled.ScrollView.attrs({
   margin-top: ${RFPercentage(20)}px;
 `;
 
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin-bottom: 16px;
+`;
+
+export const TrnsactionsList = styled(
+  FlatList as new () => FlatList<DataListProps>).attrs({
+    showsVerticalScrollIndicator: false,
+    paddingBottom: Platform.OS === 'ios' ? getBottomSpace() : 0
+  })``;
