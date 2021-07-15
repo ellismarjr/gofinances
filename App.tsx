bd69211a7cd6944dcdd7b80125ed397a68cded3b
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-15 10:14:58 -0300

    chore: add StatusBar light

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 
 import {
   useFonts,
@@ -31,6 +32,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor={theme.colors.primary}/>
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
