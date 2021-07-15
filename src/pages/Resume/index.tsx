42e8ebbb86028e85c95a35d003a466a1644f9ea8
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-15 08:46:49 -0300

    chore: add graphics on resume

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,7 +1,13 @@
 import React, { useCallback, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native'
+import { useFocusEffect } from '@react-navigation/core';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 import { HistoryCard } from '../../components/HistoryCard';
+import { ITransactionCard } from '../../components/TransactionCard';
+import { EmptyMessage } from '../../components/EmptyMessage';
+
 import { asyncStorageDataKey } from '../../shared/constants';
 
 import { categories } from '../../utils/categories';
@@ -13,21 +19,23 @@ import {
   Container,
   Header,
   Title,
-  Content
+  Content,
+  ChartContainer
 } from './styles';
-import { ScrollView } from 'react-native';
-import { EmptyMessage } from '../../components/EmptyMessage';
-import { useFocusEffect } from '@react-navigation/core';
+import { useTheme } from 'styled-components';
 
 interface CategoryData {
   id: string;
   name: string;
-  total: string;
+  total: number;
+  totalFormatted: string;
   color: string;
+  percent: string;
 }
 
 export function Resume() {
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+  const theme = useTheme();
 
   async function loadData() {
     const response = await AsyncStorage.getItem(asyncStorageDataKey);
@@ -35,6 +43,11 @@ export function Resume() {
     const responseParsed: Transaction[] = response ? JSON.parse(response) : [];
 
     const expensives = responseParsed.filter(expensive => expensive.type === 'negative');
+
+    const expensivesTotal = expensives.reduce((acc: number, expensive: ITransactionCard) => {
+      return acc + Number(expensive.amount)
+    }, 0);
+
 
     const totalByCategory: CategoryData[] = [];
 
@@ -48,12 +61,18 @@ export function Resume() {
       });
 
       if (categorySum > 0) {
+        const percent = `${(categorySum / expensivesTotal * 100).toFixed(0)}%`;
+
         totalByCategory.push({
           id: category.key,
           name: category.name,
-          total: currencyFormat(categorySum),
-          color: category.color
+          total: categorySum,
+          totalFormatted: currencyFormat(categorySum),
+          color: category.color,
+          percent
         });
+
+
       }
     });
 
@@ -74,11 +93,27 @@ export function Resume() {
       {totalByCategories.length > 0
         ? (
           <Content>
+            <ChartContainer>
+              <VictoryPie
+                data={totalByCategories}
+                colorScale={totalByCategories.map(category => category.color)}
+                style={{
+                  labels: {
+                    fontSize: RFValue(18),
+                    fontWeight: 'bold',
+                    fill: theme.colors.shape
+                  }
+                }}
+                labelRadius={50}
+                x="percent"
+                y="totalFormatted"
+              />
+            </ChartContainer>
             {totalByCategories.map(category => (
               <HistoryCard
                 key={category.id}
                 title={category.name}
-                amount={category.total}
+                amount={category.totalFormatted}
                 color={category.color}
               />
             ))}
