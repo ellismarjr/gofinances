3e5941b0bb726dac514c396f279f062d90c6f67d
Junior de Oliveira <ellismarjr@hotmail.com>
2021-12-13 21:46:48 -0300

    feat: oauth google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import * as AuthSession from 'expo-auth-session';
 import { createContext, ReactNode, useState } from 'react';
 
 type User = {
@@ -10,6 +11,7 @@ type User = {
 
 type AuthContextData = {
   user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
 };
 
 type AuthProviderProps = {
@@ -26,10 +28,29 @@ export function AuthProvider({ children }: AuthProviderProps) {
     email: 'júnior@email.com',
   }
 
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID = '829840799250-mtnn1v8a94uid973lnf574sk94mau39t.apps.googleusercontent.com';
+      const REDIRECT_URI = 'https://auth.expo.io/@ellismarjr/gofinances';
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('profile email');
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const response = await AuthSession.startAsync({ authUrl });
+      console.log(response);
+
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
 
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{
+      user,
+      signInWithGoogle
+    }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
 
+import { useAuth } from '../../hooks/useAuth';
+
 import { SignInSocialButton } from '../../components/SignInSocialButton';
 
 import AppleSvg from '../../assets/images/apple.svg';
@@ -16,13 +18,25 @@ import {
   Footer,
   FooterWrapper
 } from './styles';
+import { Alert } from 'react-native';
 
-export function SignIn(){
+export function SignIn() {
+  const { signInWithGoogle } = useAuth();
+
+  async function handleSignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível conectar a conta Google');
+    }
+  }
+
   return (
     <Container>
       <Header>
         <TitleWrapper>
-          <LogoSvg 
+          <LogoSvg
             width={RFValue(120)}
             height={RFValue(68)}
           />
@@ -42,8 +56,11 @@ export function SignIn(){
 
       <Footer>
         <FooterWrapper>
-          <SignInSocialButton title="Entrar com Google" svg={GoogleSvg} />
-          <SignInSocialButton title="Entrar com Apple" svg={AppleSvg}/>
+          <SignInSocialButton
+            title="Entrar com Google"
+            svg={GoogleSvg}
+            onPress={handleSignInWithGoogle} />
+          <SignInSocialButton title="Entrar com Apple" svg={AppleSvg} />
         </FooterWrapper>
       </Footer>
     </Container>
