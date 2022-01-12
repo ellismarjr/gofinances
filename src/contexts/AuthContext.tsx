4cd88be562e2740931399fdd051459c25be69486
Júnior de Oliveira <ellismarjr@hotmail.com>
2022-01-12 10:21:55 -0300

    feat: signin with google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import * as AuthSession from 'expo-auth-session';
 import { createContext, ReactNode, useState } from 'react';
 
+const {CLIENT_ID} = process.env;
+const {REDIRECT_URI} = process.env;
+
 type User = {
   id: string;
   name: string;
@@ -18,28 +21,40 @@ type AuthProviderProps = {
   children: ReactNode;
 };
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  },
+  type: string;
+}
+
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  // const [user, setUser] = useState<User>();
-  const user = {
-    id: '123',
-    name: 'Júnior',
-    email: 'júnior@email.com',
-  }
+  const [user, setUser] = useState<User>({} as User);
+  
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID = '829840799250-mtnn1v8a94uid973lnf574sk94mau39t.apps.googleusercontent.com';
-      const REDIRECT_URI = 'https://auth.expo.io/@ellismarjr/gofinances';
       const RESPONSE_TYPE = 'token';
       const SCOPE = encodeURI('profile email');
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const response = await AuthSession.startAsync({ authUrl });
-      console.log(response);
+      const {type,params} = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
 
-    } catch (error) {
+      if (type === 'success') {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+        
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture
+        });
+      }
+
+    } catch (error: any) {
       throw new Error(error);
     }
   }
